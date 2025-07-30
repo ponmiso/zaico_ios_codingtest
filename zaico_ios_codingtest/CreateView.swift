983b9584967b888ecd91109d506ec5baf9931a66
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-07-30 17:16:18 +0900

    在庫データ作成画面のUIを作成

--- zaico_ios_codingtest/CreateView.swift
+++ zaico_ios_codingtest/CreateView.swift
@@ -1,8 +1,29 @@
 import SwiftUI
 
 struct CreateView: View {
+    @State private var title: String = ""
+    
     var body: some View {
-        Text("CreateView")
+        VStack(alignment: .leading) {
+            Group {
+                Text("タイトル")
+                TextField("", text: $title)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding(.horizontal, 16)
+            
+            Spacer()
+            
+            Button {
+                print("tapped button: \(title)")
+            } label: {
+                Text("作成する")
+                    .frame(height: 44)
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.white)
+                    .background(.orange)
+            }
+        }
     }
 }
 
