d064e28a34531928ae820c03dd7d6673ff370070
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-07-30 17:53:39 +0900

    在庫データ作成後にアラートを表示する

--- zaico_ios_codingtest/CreateView.swift
+++ zaico_ios_codingtest/CreateView.swift
@@ -4,6 +4,9 @@ struct CreateView: View {
     @State private var title: String = ""
     @Environment(\.dismiss) var dismiss
     
+    @State private var isPresentedAlert = false
+    @State private var alertDetails: AlertDetails?
+    
     var body: some View {
         VStack(alignment: .leading) {
             Group {
@@ -25,23 +28,53 @@ struct CreateView: View {
                     .background(.orange)
             }
         }
+        .alert("", isPresented: $isPresentedAlert, presenting: alertDetails) { details in
+            Button("OK") {
+                switch details.type {
+                case .success:
+                    dismiss()
+                case .error:
+                    break
+                }
+            }
+        } message: { details in
+            switch details.type {
+            case let .success(message), let .error(message):
+                Text(message)
+            }
+        }
     }
 }
 
 extension CreateView {
     private func createInventories() {
         if title.isEmpty {
-            print("title is empty")
+            alertDetails = AlertDetails(type: .error("タイトルを入力してください"))
+            isPresentedAlert = true
             return
         }
         Task {
             do {
                 try await APIClient.shared.createInventories(title: title)
-                dismiss()
+                alertDetails = AlertDetails(type: .success("在庫データの作成が完了しました"))
+                isPresentedAlert = true
             } catch {
-                print(error.localizedDescription)
+                alertDetails = AlertDetails(type: .error("登録に失敗しました：\(error.localizedDescription)"))
+                isPresentedAlert = true
             }
         }
+    }
+}
+
+extension CreateView {
+    struct AlertDetails: Identifiable {
+        let id = UUID()
+        let type: AlertType
+    }
+
+    enum AlertType {
+        case success(String)
+        case error(String)
     }
 }
 
