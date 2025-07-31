ed12251c5234254d4503a187859f966b9c082286
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-07-31 14:39:28 +0900

    APIのリクエスト処理をAPIClientに移行

--- zaico_ios_codingtest/API/APIClient.swift
+++ zaico_ios_codingtest/API/APIClient.swift
@@ -10,3 +10,39 @@ import Foundation
 struct APIClient {
     static let token = Secret.token // 実際のトークンに置き換える
 }
+
+extension APIClient {
+    @discardableResult
+    static func request(endpoint: Endpoint, headerFields: [String: String] = [:], httpBody: Data? = nil) async throws -> Data {
+        guard let url = URL(string: endpoint.urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.setValue("Bearer \(APIClient.token)", forHTTPHeaderField: "Authorization")
+        headerFields.forEach { value, field in
+            request.setValue(value, forHTTPHeaderField: field)
+        }
+        request.httpBody = httpBody
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if !(200...299).contains(httpResponse.statusCode) {
+                    throw URLError(.badServerResponse)
+                }
+            }
+
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("[APIClient] API Response: \(jsonString)")
+            }
+            
+            return data
+        } catch {
+            throw error
+        }
+    }
+}
+
--- zaico_ios_codingtest/API/InventoriesAPI.swift
+++ zaico_ios_codingtest/API/InventoriesAPI.swift
@@ -9,7 +9,7 @@ protocol InventoriesAPIProtocol {
 struct InventoriesAPI: InventoriesAPIProtocol {
     func fetchInventories() async throws -> [Inventory] {
         do {
-            let response = try await request(endpoint: .inventories)
+            let response = try await APIClient.request(endpoint: .inventories)
             return try JSONDecoder().decode([Inventory].self, from: response)
         } catch {
             throw error
@@ -22,7 +22,7 @@ struct InventoriesAPI: InventoriesAPIProtocol {
         }
         
         do {
-            let response = try await request(endpoint: .inventory(id: id))
+            let response = try await APIClient.request(endpoint: .inventory(id: id))
             return try JSONDecoder().decode(Inventory.self, from: response)
         } catch {
             throw error
@@ -38,42 +38,7 @@ struct InventoriesAPI: InventoriesAPIProtocol {
         }
         
         do {
-            try await request(endpoint: .createInventory, headerFields: headerFields, httpBody: httpBody)
-        } catch {
-            throw error
-        }
-    }
-}
-
-extension InventoriesAPI {
-    @discardableResult
-    private func request(endpoint: Endpoint, headerFields: [String: String] = [:], httpBody: Data? = nil) async throws -> Data {
-        guard let url = URL(string: endpoint.urlString) else {
-            throw URLError(.badURL)
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = endpoint.method.rawValue
-        request.setValue("Bearer \(APIClient.token)", forHTTPHeaderField: "Authorization")
-        headerFields.forEach { value, field in
-            request.setValue(value, forHTTPHeaderField: field)
-        }
-        request.httpBody = httpBody
-        
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                if !(200...299).contains(httpResponse.statusCode) {
-                    throw URLError(.badServerResponse)
-                }
-            }
-
-            if let jsonString = String(data: data, encoding: .utf8) {
-                print("[APIClient] API Response: \(jsonString)")
-            }
-            
-            return data
+            try await APIClient.request(endpoint: .createInventory, headerFields: headerFields, httpBody: httpBody)
         } catch {
             throw error
         }
